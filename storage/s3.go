b5a0dcc410be5e3574e790608d43a87589019641
Tommaso Gragnato <tommaso.gragnato@immobiliare.it>
2024-03-07 22:49:26 +0100

    fix(storage): missing validation for s3 bucket names

--- storage/s3.go
+++ storage/s3.go
@@ -2,8 +2,10 @@ package storage
 
 import (
 	"bytes"
+	"errors"
 	"fmt"
 	"io"
+	"net"
 	"strings"
 
 	"github.com/aws/aws-sdk-go/aws"
@@ -64,6 +66,11 @@ func (s *S3) Tune(options map[string]interface{}) error {
 }
 
 func (s *S3) Get(name string) ([]byte, []byte, error) {
+	bucketName, err := nameToBucket(name)
+	if err != nil {
+		return nil, nil, err
+	}
+
 	client := s3.New(
 		session.Must(session.NewSession()),
 		s.config,
@@ -71,7 +78,7 @@ func (s *S3) Get(name string) ([]byte, []byte, error) {
 
 	crtData := bytes.NewBuffer(nil)
 	if data, err := client.GetObject(&s3.GetObjectInput{
-		Bucket: nameToBucket(name),
+		Bucket: bucketName,
 		Key:    &s3CrtName,
 	}); err != nil {
 		return nil, nil, err
@@ -84,7 +91,7 @@ func (s *S3) Get(name string) ([]byte, []byte, error) {
 
 	keyData := bytes.NewBuffer(nil)
 	if data, err := client.GetObject(&s3.GetObjectInput{
-		Bucket: nameToBucket(name),
+		Bucket: bucketName,
 		Key:    &s3KeyName,
 	}); err != nil {
 		return nil, nil, err
@@ -99,19 +106,24 @@ func (s *S3) Get(name string) ([]byte, []byte, error) {
 }
 
 func (s *S3) Put(name string, crtData, keyData []byte) error {
+	bucketName, err := nameToBucket(name)
+	if err != nil {
+		return err
+	}
+
 	client := s3.New(
 		session.Must(session.NewSession()),
 		s.config,
 	)
 
-	if _, err := client.CreateBucket(&s3.CreateBucketInput{Bucket: nameToBucket(name)}); err != nil &&
+	if _, err := client.CreateBucket(&s3.CreateBucketInput{Bucket: bucketName}); err != nil &&
 		strings.HasPrefix(err.Error(), s3.ErrCodeBucketAlreadyExists) &&
 		strings.HasPrefix(err.Error(), s3.ErrCodeBucketAlreadyOwnedByYou) {
 		return err
 	}
 
 	if _, err := client.PutObject(&s3.PutObjectInput{
-		Bucket: nameToBucket(name),
+		Bucket: bucketName,
 		Key:    &s3CrtName,
 		Body:   bytes.NewReader(crtData),
 	}); err != nil {
@@ -119,7 +131,7 @@ func (s *S3) Put(name string, crtData, keyData []byte) error {
 	}
 
 	if _, err := client.PutObject(&s3.PutObjectInput{
-		Bucket: nameToBucket(name),
+		Bucket: bucketName,
 		Key:    &s3KeyName,
 		Body:   bytes.NewReader(keyData),
 	}); err != nil {
@@ -130,6 +142,11 @@ func (s *S3) Put(name string, crtData, keyData []byte) error {
 }
 
 func (s *S3) Del(name string) error {
+	bucketName, err := nameToBucket(name)
+	if err != nil {
+		return err
+	}
+
 	client := s3.New(
 		session.Must(session.NewSession()),
 		s.config,
@@ -138,13 +155,13 @@ func (s *S3) Del(name string) error {
 	if err := s3manager.NewBatchDeleteWithClient(client).Delete(
 		aws.BackgroundContext(),
 		s3manager.NewDeleteListIterator(client, &s3.ListObjectsInput{
-			Bucket: nameToBucket(name),
+			Bucket: bucketName,
 		})); err != nil {
 		return err
 	}
 
 	if _, err := client.DeleteBucket(&s3.DeleteBucketInput{
-		Bucket: nameToBucket(name),
+		Bucket: bucketName,
 	}); err != nil {
 		return err
 	}
@@ -165,8 +182,7 @@ func (s *S3) Find(filters ...string) ([][]byte, error) {
 
 	results := [][]byte{}
 	for _, bucket := range buckets.Buckets {
-		if !(pki.IsValidCN(bucketToName(bucket.Name)) &&
-			util.RegexesMatch(bucketToName(bucket.Name), filters...)) {
+		if !matchFilters(bucket.Name, filters) {
 			continue
 		}
 
@@ -188,11 +204,87 @@ func (s *S3) Config() map[string]string {
 	}
 }
 
-func nameToBucket(name string) *string {
-	bucket := strings.ReplaceAll(name, ".", "-")
-	return &bucket
+func nameToBucket(name string) (*string, error) {
+	bucket := strings.TrimPrefix(name, "*.")
+	if !validateBucketName(bucket) {
+		return nil, errors.New("unsupported CN (protocol violation)")
+	}
+
+	// Buckets used with Amazon S3 Transfer Acceleration can't have dots (.) in their names
+	bucket = strings.ReplaceAll(bucket, ".", "-")
+	return &bucket, nil
 }
 
-func bucketToName(bucket *string) string {
-	return strings.ReplaceAll(*bucket, "-", ".")
+func matchFilters(bucket *string, filters []string) bool {
+	name := strings.ReplaceAll(*bucket, "-", ".")
+	wildcardName := "*." + name
+
+	return pki.IsValidCN(name) && util.RegexesMatch(name, filters...) ||
+		pki.IsValidCN(wildcardName) && util.RegexesMatch(wildcardName, filters...)
+}
+
+func validateBucketName(name string) bool {
+
+	// Bucket names cannot be formatted as IP addresses
+	if net.ParseIP(name) != nil {
+		return false
+	}
+
+	// Bucket names must not start with the prefix xn--
+	if strings.HasPrefix(name, "xn--") {
+		return false
+	}
+
+	// Bucket names must not start with the prefix sthree-
+	if strings.HasPrefix(name, "sthree-") {
+		return false
+	}
+
+	// Bucket names must not end with the suffix -s3alias
+	if strings.HasSuffix(name, "-s3alias") {
+		return false
+	}
+
+	// Bucket names must not end with the suffix --ol-s3
+	if strings.HasSuffix(name, "--ol-s3") {
+		return false
+	}
+
+	// Bucket names can be between 3 and 63 characters long
+	if len(name) < 3 || len(name) > 63 {
+		return false
+	}
+
+	// Bucket names must not contain uppercase characters
+	if name != strings.ToLower(name) {
+		return false
+	}
+
+	// Bucket names must not contain underscores
+	if strings.Contains(name, "_") {
+		return false
+	}
+
+	// Bucket names must be a series of one or more labels
+	for _, label := range strings.Split(name, ".") {
+
+		// Adjacent labels are separated by a single period (.)
+		if len(label) < 1 {
+			return false
+		}
+
+		// Each label can contain lowercase letters, numbers, and hyphens
+		for _, char := range label {
+			if !('a' <= char && char <= 'z') && !('0' <= char && char <= '9') && char != '-' {
+				return false
+			}
+		}
+
+		// Each label must start and end with a lowercase letter or a number
+		if label[0] == '-' || label[len(label)-1] == '-' {
+			return false
+		}
+	}
+
+	return true
 }
