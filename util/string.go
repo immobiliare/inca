d6a58e0b84568c4322b30445deb34ef61fa2d0d2
Tommaso Gragnato <tommaso.gragnato@immobiliare.it>
2025-06-20 18:22:01 +0200

    feat(util): add GenerateRandomString functionality

--- util/string.go
+++ util/string.go
@@ -1,6 +1,28 @@
 package util
 
-import "regexp"
+import (
+	"crypto/rand"
+	"math/big"
+	mrand "math/rand/v2"
+	"regexp"
+)
+
+const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*"
+
+func GenerateRandomString(length int) string {
+	result := make([]byte, length)
+	charsetLength := big.NewInt(int64(len(charset)))
+
+	for i := 0; i < length; i++ {
+		randomIndex, err := rand.Int(rand.Reader, charsetLength)
+		if err != nil {
+			randomIndex = big.NewInt(int64(mrand.IntN(len(charset))))
+		}
+		result[i] = charset[randomIndex.Int64()]
+	}
+
+	return string(result)
+}
 
 func RegexesMatch(payload string, regexes ...string) bool {
 	match := true
--- util/string_test.go
+++ util/string_test.go
@@ -43,3 +43,39 @@ func TestUtilStringRegexesNotAnyMatch(t *testing.T) {
 
 	is.New(t).True(!RegexesAnyMatch(testingString, "^$"))
 }
+
+func TestGenerateRandomString_Length(t *testing.T) {
+	t.Parallel()
+	is := is.New(t)
+
+	for _, l := range []int{0, 1, 5, 10, 50, 100} {
+		s := GenerateRandomString(l)
+		is.Equal(len(s), l)
+	}
+}
+
+func TestGenerateRandomString_Charset(t *testing.T) {
+	t.Parallel()
+	is := is.New(t)
+
+	s := GenerateRandomString(1000)
+	for _, c := range s {
+		found := false
+		for _, allowed := range charset {
+			if c == allowed {
+				found = true
+				break
+			}
+		}
+		is.True(found)
+	}
+}
+
+func TestGenerateRandomString_Uniqueness(t *testing.T) {
+	t.Parallel()
+	is := is.New(t)
+
+	s1 := GenerateRandomString(32)
+	s2 := GenerateRandomString(32)
+	is.True(s1 != s2)
+}
